2f655d0166cc76ed999288dce011ee95f4d2cd29
Yogeshwar Ghule <59093827+yogeshwarghule@users.noreply.github.com>
2022-09-01 22:42:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask, render_template
-app = Flask(__name__,template_folder='templateFiles')
+from flask import Flask
+app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return "Welcome to Signiance Techologies"
 
-
+@app.route('/about')
+def hello():
+    return 'Helping startups to cut infrastructure costs, boost productivity, & manage the workload effectively on cloud & DevOps'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
